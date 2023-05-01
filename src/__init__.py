9a169a93e9904232734b2c26290f524b8100dcd7
Koko <pointmisser@gmail.com>
2023-05-01 15:16:28 +0100

    Add comments and correct typo.

--- src/__init__.py
+++ src/__init__.py
@@ -9,15 +9,15 @@ from src.database import db
 
 # create_app is the factory function
 def create_app(test_config=None):
-    """Create main app entry poin"""
-    #
+    """Create main app entry point"""
+    # create and configure the app
     app = Flask(__name__,
                 instance_relative_config=True)
-    #
+    # load the instance config, if it exists, when not testing
     if test_config is None:
         app.config.from_mapping(
             SECRET_KEY=os.environ.get("SECRET_KEY"),
-            SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEY_DB_URI")
+            SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DB_URI")
             )
     # if test_config is not None, then the app is in test mode
     else:
@@ -31,3 +31,30 @@ def create_app(test_config=None):
     app.register_blueprint(bookmarks)
 
     return app
+
+# def create_app(test_config=None):
+#     # create and configure the app
+#     app = Flask(__name__, instance_relative_config=True)
+#     app.config.from_mapping(
+#         SECRET_KEY=os.environ.get("SECRET_KEY"),
+#         DATABASE=os.path.join(app.instance_path, 'src.sqlite'),
+#     )
+
+#     if test_config is None:
+#         # load the instance config, if it exists, when not testing
+#         app.config.from_pyfile('config.py', silent=True)
+#     else:
+#         # load the test config if passed in
+#         app.config.from_mapping(test_config)
+
+#     # ensure the instance folder exists
+#     try:
+#         os.makedirs(app.instance_path)
+#     except OSError:
+#         pass
+
+#     # register Blueprints in the app instance
+#     app.register_blueprint(auth)
+#     app.register_blueprint(bookmarks)
+
+#     return app
